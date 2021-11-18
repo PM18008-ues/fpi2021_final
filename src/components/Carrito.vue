f32208145b9057368f01f255d62cbf516a3e8f43
Melvin Portillo <63684365+PM18008-ues@users.noreply.github.com>
2021-11-18 13:09:37 -0600

    foto producto en carrito
    Se agrego la foto del producto en el carrito

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -22,12 +22,20 @@
                 <template v-slot:default>
                   <thead>
                     <tr>
+                      <th class="text-left">Imagen</th>
                       <th class="text-left">Producto</th>
                       <th class="text-left">Precio</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr v-for="(item, key) in compras" :key="key">
+                      <td>
+                        <v-img
+                          :src="item.imagenes[0]"
+                          max-height="130"
+                          max-width="130"
+                        ></v-img>
+                      </td>
                       <td>{{ item.titulo }}</td>
                       <td>${{ item.precio }}</td>
                     </tr>
